4ccc4eb195850c4d692a7830ff19009ad85fd4b3
jennywoon <cjaeks0394@naver.com>
2022-08-29 18:34:26 +0900

    react-responsive 테스트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Router from "./shared/Router";
-import { BrowserView, MobileView } from 'react-device-detect'
+import { useMediaQuery } from "react-responsive"
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  const isPc = useMediaQuery({
+    query: "(min-width:1024px)"
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)"
+  });
+
   return (
     <>
       {/* <Router /> */}
-      <BrowserView><Router/></BrowserView>
-      <MobileView><Router/></MobileView>
+      {isPc && <Router />}
+      {isTablet && <Router />}
+      {isMobile && <Router />}
     </>
   )
 }
