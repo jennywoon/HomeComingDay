b3e561cbf443f9ae299fa197d0576da9dc405723
hyeongyong-choi <guddyd6761@naver.com>
2022-08-29 14:33:23 +0900

    0829시작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import GoogleLogin from "./components/GoogleLogin";
 import Router from "./shared/Router";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 
 function App() {
-  return <Router />;
+  return 
+  <Router />;
+  
+  
 }
 
 export default App;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+<<<<<<< Updated upstream
 import NaverLogin from "../components/NaverLogin";
+=======
+import GoogleLogin from "../components/GoogleLogin";
+>>>>>>> Stashed changes
 
 const Router = () => {
 
@@ -8,7 +12,12 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< Updated upstream
         <Route path="/naverlogin" element={<NaverLogin />} />
+=======
+      <Route path="/" element={<GoogleLogin />} />
+        {/* <Route path="/main" element={<MainPage />} /> */}
+>>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
