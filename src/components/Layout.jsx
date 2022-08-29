4fe1451afc41aa4fa87df7ee8ada58c512a797c1
jennywoon <cjaeks0394@naver.com>
2022-08-29 20:03:22 +0900

    height 조정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -27,7 +27,7 @@ const LayoutContainer = styled.div`
 
 const LayoutWrap = styled.div`
     width: 420px;
-    height: 1190px;
+    height: 100vh;
     border: 1px solid blue;
     background-color: white;
     display: flex;
