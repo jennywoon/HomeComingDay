d789e96b21f43bec275bfb8ad72643d3b2c8cc2f
jennywoon <109018926+jennywoon@users.noreply.github.com>
2022-09-06 02:00:31 +0900

    [style] 캘린더모달 반응형 구현

--- src/components/calendarBoard/CalendarModal.jsx
+++ src/components/calendarBoard/CalendarModal.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Calendar from 'react-calendar';
 import "./CalendarModal.css"
@@ -15,11 +15,10 @@ const CalendarModal = ({ setModalOpen }) => {
     // const {dates} = useSelector((state) => state.dates)
     // console.log(dates);
 
-
     const [date, setDate] = useState({
-        calendarDate : "",
+        calendarDate: "",
     });
-    const {calendarDate} = date;
+    const { calendarDate } = date;
     const realCalendar = date.toString();
 
     const modalRef = useRef(null);
@@ -41,31 +40,36 @@ const CalendarModal = ({ setModalOpen }) => {
 
     // const {clikedDay} = value;
 
-    const onsubmitHandler = (e) =>{
+    const onsubmitHandler = (e) => {
         e.preventDefault();
-        const dates = {calendarDate : realCalendar}
+        const dates = { calendarDate: realCalendar }
         dispatch(__postDate(dates));
     }
 
     return (
         <Background onClick={closeModal}>
-            <Container >
-                <Wrap>
-                    <CalendarWrap ref={modalRef}>
-                        <Calendar
-                            onChange={onChange}
-                            // calendarDate={calendarDate} 
-                            // name="calendarDate"
-                            // value={calendarDate}
-                            formatDay={(locale, date) => dayjs(date).format('DD')}
-                        />
-                        <CalendarButton
-                        onClick={onsubmitHandler}
-                        >확인</CalendarButton>
-                        {moment(date).format("YYYY년 MM월 DD일")}
-                    </CalendarWrap>
-                </Wrap>
-            </Container>
+            {/* <ModalContainer> */}
+                <Container >
+                {/* <ModalContainer> */}
+                    {/* <div className='wrap' > */}
+                        <Wrap >
+                            <CalendarWrap ref={modalRef}>
+                                <Calendar
+                                    onChange={onChange}
+                                    // calendarDate={calendarDate} 
+                                    // name="calendarDate"
+                                    // value={calendarDate}
+                                    formatDay={(locale, date) => dayjs(date).format('DD')}
+                                />
+                                <CalendarButton
+                                    onClick={onsubmitHandler}
+                                >확인</CalendarButton>
+                                {moment(date).format("YYYY년 MM월 DD일")}
+                            </CalendarWrap>
+                        </Wrap>
+                    {/* </div> */}
+                    {/* </ModalContainer> */}
+                </Container>
         </Background>
     );
 };
@@ -74,7 +78,6 @@ export default CalendarModal;
 
 const Background = styled.div`
     position: fixed;
-    /* position: absolute; */
     top: 0;
     left: 0;
     bottom: 0;
@@ -84,33 +87,55 @@ const Background = styled.div`
 `
 
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-  /* background-color: rgba(87,87,87,0.5); */
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-    height: 100%;
-    display: flex; 
-    justify-content: center;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  /* align-items: center; */
+  /* height: 100vh; */
+  width: 100%;
+  /* height: 100%; */
+  /* background-color: #f7ede2; */
+  overflow: hidden;
+  @media screen and (max-width: 1024px) {
+    background-image: none;
   }
-`
+  // 모바일 뷰
+  .wrap {
+    position: relative;
+    width: 100%;
+    /* max-height: 1000px; */
+    max-height: 1202px;
+    max-width: 420px;
+    margin: auto;
+    /* background-color: var(--white); */
+    /* background-color: white; */
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 24 px;
+    @media screen and (min-width: 1024px) {
+      /* position: relative; */
+      left: 15%;
+      top: 0%;
+      overflow: auto;
+    }
+  }
+`;
 
 const Wrap = styled.div`
-    width: 500px;
-    /* height: 100vh; */
+    position: relative;
+    width: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: flex-end;
     background-color: rgba(87,87,87,0.3);
+    overflow-y: hidden;
+    /* border: 1px solid red; */
 `
 
 const CalendarWrap = styled.div`
-    /* padding: 0 20px; */
-    /* width: 460px; */
     width: 100%;
-    height: 450x;
+    height: 450px;
     background-color: white;
     display: flex;
     flex-direction: column;
