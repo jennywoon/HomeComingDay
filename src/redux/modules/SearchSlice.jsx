234b9ef7aa8f2de707c1e101ce133c1be834ae9d
hyeongyong-choi <guddyd6761@naver.com>
2022-09-06 13:56:07 +0900

    [feat]검색기능작업중

--- src/redux/modules/SearchSlice.jsx
+++ src/redux/modules/SearchSlice.jsx
@@ -7,21 +7,31 @@ import axios from "axios";
 const initialState = {
     searchs: [  
     ],
+    helps: [],
     // insta: null,
     isLoading: false,
     error: null,
 };
 
-export const __getSearch = createAsyncThunk("searchs/getSearch", async (payload, thunkAPI) => {
-    try {
-        const data = await axios.get("http://localhost:3001/searchs")
-        console.log(data.data)
-        return thunkAPI.fulfillWithValue(data.data);
-    } catch (error) {
-        console.log('error', error);
-        return thunkAPI.rejectWithValue(error);
-    }
-});
+// export const __getSearch = createAsyncThunk("searchs/getSearch", async (payload, thunkAPI) => {
+//     try {
+//         const data = await axios.get("http://localhost:3001/searchs")
+//         console.log(data.data)
+//         return thunkAPI.fulfillWithValue(data.data);
+//     } catch (error) {
+//         console.log('error', error);
+//         return thunkAPI.rejectWithValue(error);
+//     }
+// });
+
+export const __getSearch = createAsyncThunk("searchs/getsearchs", async (props)=>{
+  console.log(props)
+  return await axios
+  .get(`http://localhost:3001/helps?keyword=${props}` )
+  .then((response) => response.data)
+})
+
+
 
 export const __postSearch = createAsyncThunk("searchs/postSearch", async (payload, thunkAPI) => {
     console.log('payload', payload)
@@ -39,23 +49,37 @@ export const SearchSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: {
-      [__getSearch.pending]: (state) => {
-        state.isLoading = true;
-      },
+      // [__getSearch.pending]: (state) => {
+      //   state.isLoading = true;
+      // },
+      // [__getSearch.fulfilled]: (state, action) => {
+      //   state.isLoading = false;
+      //   state.searchs = action.payload;
+      // },
+      // [__getSearch.rejected]: (state, action) => {
+      //   state.isLoading = false;
+      //   state.error = action.payload;
+      // },
+
       [__getSearch.fulfilled]: (state, action) => {
-        state.isLoading = false;
-        state.searchs = action.payload;
+        console.log(state.helps)
+        console.log(action.payload)
+        state.helps = action.payload.title;
+        // state.result_musicArtist = action.payload.musicArtist;
+        // state.result_musicTitle = action.payload.musicTitle;
+        // state.result_videoArtist = action.payload.videoArtist;
+        // state.result_videoTitle = action.payload.videoTitle;
+  
       },
       [__getSearch.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
       },
+
       [__postSearch.pending]: (state) => {
         state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
       },
       [__postSearch.fulfilled]: (state, action) => {
         state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
-        state.searchs.push(action.payload); // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+        // state.searchs.push(action.payload); // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
       },
       [__postSearch.rejected]: (state, action) => {
         state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
