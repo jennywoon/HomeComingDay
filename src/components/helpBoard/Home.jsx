51e5c21399789aaaf56db75a8f0f5da8f6c5206d
jennywoon <109018926+jennywoon@users.noreply.github.com>
2022-09-30 23:58:00 +0900

    [fix] 채팅 버그 해결 중

--- src/components/helpBoard/Home.jsx
+++ src/components/helpBoard/Home.jsx
@@ -12,8 +12,8 @@ import Myimg from '../../assets/My.png';
 const Home = () => {
   const navigate = useNavigate();
   
-  // const chatList = useSelector((state) => state.chat.chatList[0]);
-  // console.log(chatList);
+  const chatList = useSelector((state) => state.chat.chatList[0]);
+  console.log(chatList);
 
   return (
     <StHomeContainer>
@@ -48,11 +48,11 @@ const Home = () => {
           >
             <StChatIconWrap>
               <StImg src={Chatimg} alt='채팅' />
-              {/* {chatList && chatList.totalCnt > 0 ? (
+              {chatList && chatList.totalCnt > 0 ? (
                 <StNewDiv>
                 <StNewTitle>N</StNewTitle>
               </StNewDiv>
-              ) : null} */}
+              ) : null}
             </StChatIconWrap>
             <StTapTitle>채팅</StTapTitle>
           </StTap>
